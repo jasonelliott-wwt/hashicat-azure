99be8f5e4418bd0791058492f1ec79013ec1dc1f
Jason Elliott <jason.elliott@wwt.com>
2020-06-23 21:35:02 +0000

    updates rev3

--- main.tf
+++ main.tf
@@ -7,23 +7,9 @@ resource "azurerm_resource_group" "myresourcegroup" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.myresourcegroup.location
-  address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.prefix}-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.myresourcegroup.name
-  address_prefix       = var.subnet_prefix
-}
-
 resource "azurerm_network_security_group" "catapp-sg" {
   name                = "${var.prefix}-sg"
-  location            = var.location
+  location            = azurerm_resource_group.myresourcegroup.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 
   security_rule {
